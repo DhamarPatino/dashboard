0afaf6178012952bff6c26b1fb0283b9ba63d6f8
dalfonzo21 <149984303+dalfonzo21@users.noreply.github.com>
2025-07-07 15:48:10 +0000

    .

--- src/App.tsx
+++ src/App.tsx
@@ -3,35 +3,76 @@ import { Grid } from '@mui/material';
 import HeaderUI from './components/HeaderUI';
 import AlertUI from './components/AlertUI';
 import SeclectorUI from './components/SelectorUI';
-function App() {
+import IndicatorUI from './components/IndicatorUI';
+import DataFetcher from './functions/DataFetcher';
 
-  return (
+function App() {
+   const dataFetcherOutput = DataFetcher();
+
+   return (
       <Grid container spacing={5} justifyContent="center" alignItems="center">
 
          {/* Encabezado */}
          <Grid size={{ xs: 12, md: 12 }}>Elemento: Encabezado
-            <HeaderUI/>
+            <HeaderUI />
          </Grid>
 
          {/* Alertas */}
          <Grid size={{ xs: 12, md: 12 }}
-         container justifyContent="right" alignItems="center">
-         Elemento: Alertas
-         <AlertUI description="No se preveen lluvias"/>
+            container justifyContent="right" alignItems="center">
+            Elemento: Alertas
+            <AlertUI description="No se preveen lluvias" />
 
          </Grid>
 
          {/* Selector */}
-         <Grid size={{ xs: 12, md: 3  }}>Elemento: Selector
-            <SeclectorUI/>
+         <Grid size={{ xs: 12, md: 3 }}>Elemento: Selector
+            <SeclectorUI />
          </Grid>
 
          {/* Indicadores */}
-         <Grid size={{ xs: 12, md: 9 }}>Elemento: Indicadores</Grid>
+         <Grid container size={{ xs: 12, md: 9 }} >
 
+            {/* Renderizado condicional de los datos obtenidos */}
+
+                 {dataFetcherOutput.loading && <p>Cargando datos...</p>}
+                 {dataFetcherOutput.error && <p>Error: {dataFetcherOutput.error}</p>}
+                 {dataFetcherOutput.data && (
+                 <>
+
+                     {/* Indicadores con datos obtenidos */}
+
+                     <Grid size={{ xs: 12, md: 3 }} >
+                         <IndicatorUI
+                             title='Temperatura (2m)'
+                             description={dataFetcherOutput.data.current.temperature_2m + " " + dataFetcherOutput.data.current_units.temperature_2m} />
+                     </Grid>
+
+                     <Grid size={{ xs: 12, md: 3 }}>
+                         <IndicatorUI
+                             title='Temperatura aparente'
+                             description={dataFetcherOutput.data.current.apparent_temperature + " " + dataFetcherOutput.data.current_units.apparent_temperature} />
+                     </Grid>
+
+                     <Grid size={{ xs: 12, md: 3 }}>
+                         <IndicatorUI
+                             title='Velocidad del viento'
+                             description={dataFetcherOutput.data.current.wind_speed_10m + " " + dataFetcherOutput.data.current_units.wind_speed_10m} />
+                     </Grid>
+
+                     <Grid size={{ xs: 12, md: 3 }}>
+                         <IndicatorUI
+                             title='Humedad relativa'
+                             description={dataFetcherOutput.data.current.relative_humidity_2m + " " + dataFetcherOutput.data.current_units.relative_humidity_2m} />
+                     </Grid>
+
+                 </>
+                 )}
+
+         </Grid>
          {/* Gráfico */}
-         <Grid  sx={{ display: { xs: "none", md: "block"} }}
-         size={{ xs: 12, md: 6 }}>Elemento: Gráfico</Grid>
+         <Grid sx={{ display: { xs: "none", md: "block" } }}
+            size={{ xs: 12, md: 6 }}>Elemento: Gráfico</Grid>
 
          {/* Tabla */}
          <Grid size={{ xs: 12, md: 6 }} sx={{ display: { xs: "none", md: "block" } }}>Elemento: Tabla</Grid>
@@ -39,7 +80,7 @@ function App() {
          {/* Información adicional */}
          <Grid size={{ xs: 12, md: 12 }}>Elemento: Información adicional</Grid>
 
-      </Grid>  )
+      </Grid>)
 }
 
 export default App
